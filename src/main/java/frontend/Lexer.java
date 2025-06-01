ecf27397dc463a5ef5d76472ae486586985a9207
PrabhdeepSP <prabhdepsp@gmail.com>
2025-06-01 20:13:18 +0530

    added Token

--- src/main/java/frontend/Lexer.java
+++ src/main/java/frontend/Lexer.java
@@ -1,11 +1,10 @@
 package frontend;
 
-import java.util.Collections;
 import java.util.List;
 
 public class Lexer {
     // returns a list of valid tokens
     public List<Token> scanForTokens(String sourceCode) {
-        return Collections.emptyList();
+        return List.of(new Token("name", TokenType.IDENTIFIER, 3));
     }
 }
--- src/main/java/frontend/Token.java
+++ src/main/java/frontend/Token.java
@@ -1,5 +1,17 @@
 package frontend;
 
 public class Token {
+    public final String lexeme;
+    public final TokenType type;
+    public final int lineNo;
 
+    public Token(String lexeme, TokenType type, int lineNo) {
+        this.lexeme = lexeme;
+        this.type = type;
+        this.lineNo = lineNo;
+    }
+
+    public String toString() {
+        return type + " " + lexeme + ":" + lineNo;
+    }
 }
