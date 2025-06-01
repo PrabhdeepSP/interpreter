c0978f1ef7be0b0501bd55522633e558e3b19ba8
PrabhdeepSP <prabhdepsp@gmail.com>
2025-06-01 17:05:59 +0530

    added build scripts

--- src/main/java/treelox/Main.java
+++ src/main/java/treelox/Main.java
@@ -2,6 +2,10 @@ package treelox;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello gradle");
+        if (args.length != 1) {
+            System.out.println("Error: Invalid Usage");
+            System.out.println("Correct Usage: ./treelox [script_name]");
+            return;
+        }
     }
 }
